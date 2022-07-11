dfcc1f87a57e5b8b54c8ce4580a357409b5a72dc
Cristian Estarlich <c.estarlich@signa-sportsunited.com>
2022-07-11 11:36:40 +0200

    Adding folder structure

---
+++ symlinks/generate.sh
@@ -0,0 +1,19 @@
+# Vim
+ln -s -f ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/vim/.viminfo ~/.viminfo
+
+# Git
+ln -s -f ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -s -f ~/dotfiles/git/.gitignore_global ~/.gitignore_global
+
+# ZSH
+ln -s -f ~/dotfiles/bash/zsh/.zshrc ~/.zshrc
+ln -s -f ~/dotfiles/bash/zsh/.p10k.zsh ~/.p10k.zsh
+
+# vim 
+ln -s -f ~/dotfiles/vim/.viminfo ~/.viminfo 
+ln -s -f ~/dotfiles/vim/.vimrc ~/.vimrc
+
+# @TODO pending vscode.
+
+ln -s -f ~/dotfiles/iterm/com.googlecode.iterm2.plist ~/Library/Application\ Support/com.googlecode.iterm2.plist
